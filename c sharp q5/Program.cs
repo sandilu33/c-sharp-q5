15415649993d19478f0cf8b1447a15343944cc0c
L107 LAB <L107 LAB@DESKTOP-DSR3RR3>
2024-01-31 01:22:32 -0800

    Add project files.

---
+++ c sharp q5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace StudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter student name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("enter exam marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+
+            string grade;
+
+            if (marks >= 75)
+            {
+                grade = "A";
+            }
+            else if (marks >= 60)
+            {
+                grade = "B";
+            }
+            else if (marks >= 50)
+            {
+                grade = "C";
+            }
+            else if (marks >= 40)
+            {
+                grade = "D";
+            }
+            else 
+            {
+                grade = "Failed";
+            }
+            Console.WriteLine("Grade for {0}: {1}:", name,grade);
+        }
+    }
+}
